bfca6ecdbd18524207e41036fd04c7086b3ff44b
charlieclo <charlie_clo@yahoo.com>
2023-07-13 21:05:30 +0700

    add loading view

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="loading">
-    <h1>Loading</h1>
+    <div class="loader"></div>
   </div>
 </template>
 
@@ -11,7 +11,29 @@
 .loading {
   width: 100%;
   height: 100%;
-  overflow: hidden;
-  color: var(--cobbs-black);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loader {
+  border: 8px solid var(--cobbs-grey);
+  border-top: 8px solid var(--cobbs-gold);
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+@media screen and (min-width: 1024px) {
+  .loader {
+    border: 16px solid var(--cobbs-grey);
+    border-top: 16px solid var(--cobbs-gold);
+  }
 }
 </style>
